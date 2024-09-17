6dd16e4deb53e7194864912f0a60e48a6aa1aa58
KUNI115 <[sanyu4895@gmail.com>
2024-09-17 08:51:40 +0000

    secound

--- homework-app/src/component/card.jsx
+++ homework-app/src/component/card.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
     const [people,SetUser]=useState([])
   const FetchData = async () => {
     try {
-      const response = await fetch('http://localhost:8000');
+      const response = await fetch('https://homework-back.vercel.app/');
       const data = await response.json();
       return data
     } catch (error) {

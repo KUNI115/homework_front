6e742d4fe6d1d60ce2b06bbd0d7850836eb1cc92
KUNI115 <[sanyu4895@gmail.com>
2024-11-10 12:15:11 +0000

    new

--- homework-app/src/component/card.jsx
+++ homework-app/src/component/card.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
   const FetchData = async () => {
     try {
       const response = await axios.get('https://homework-back.vercel.app/');
-      const data = await response.json();
+      const data = response.data
       return data
     } catch (error) {
       console.error('データ取得エラー:', error);}}
@@ -23,7 +23,7 @@ import { useEffect, useState } from 'react';
       {people.map((person) => (
         <li key={person.email} className="flex justify-between gap-x-6 py-5">
           <div className="flex min-w-0 gap-x-4">
-            <img alt="" src={person.imageUrl} className="h-12 w-12 flex-none rounded-full bg-gray-50" />
+            <img alt="" src='https://icon-pit.com/wp-content/uploads/2020/02/person_6089.png' className="h-12 w-12 flex-none rounded-full bg-gray-50" />
             <div className="min-w-0 flex-auto">
               <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
               <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
--- homework-app/src/component/form.jsx
+++ homework-app/src/component/form.jsx
@@ -6,6 +6,7 @@ function PersonalForm(){
         name:'',
         email:'',
         role:'',
+        imageUrl:'',
         
     })
     const SendData = async (data) => {
@@ -25,72 +26,91 @@ function PersonalForm(){
     }
 
     return(
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                Username
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  
-                  <input
-                    onChange={postdataset}
-                    id="name"
-                    name="name"
-                    type="text"
-                    placeholder="janesmith"
-                    autoComplete="username"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  />
+      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+      <div className="sm:col-span-4">
+        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+          Username
+        </label>
+        <div className="mt-2">
+          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            
+            <input
+              onChange={postdataset}
+              id="name"
+              name="name"
+              type="text"
+              placeholder="janesmith"
+              autoComplete="username"
+              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            />
 
-                </div>
-              </div>
-              
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                email
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  
-                  <input
-                    onChange={postdataset}
-                    id="email"
-                    name="email"
-                    type="text"
-                    placeholder="xxx@xxx.com"
-                    autoComplete="email"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  />
-
-                </div>
-              </div>
-              
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                role
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  
-                  <input
-                    onChange={postdataset}
-                    id="role"
-                    name="role"
-                    type="text"
-                    placeholder="student"
-                    autoComplete="role"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  />
-
-                </div>
-              </div>
-              
-            </div>
-            <button type="button"  onClick={posthandle} className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">送信</button>
+          </div>
         </div>
+        
+      </div>
+      <div className="sm:col-span-4">
+        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+          email
+        </label>
+        <div className="mt-2">
+          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            
+            <input
+              onChange={postdataset}
+              id="email"
+              name="email"
+              type="text"
+              placeholder="xxx@xxx.com"
+              autoComplete="email"
+              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            />
+
+          </div>
+        </div>
+        
+      </div>
+      <div className="sm:col-span-4">
+        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+          role
+        </label>
+        <div className="mt-2">
+          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            
+            <input
+              onChange={postdataset}
+              id="role"
+              name="role"
+              type="text"
+              placeholder="student"
+              autoComplete="role"
+              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            />
+
+          </div>
+        </div>
+        <div className="sm:col-span-4">
+        <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+          img
+        </label>
+        <div className="mt-2">
+          <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+            
+            <input
+              onChange={postdataset}
+              id="imageUrl"
+              name="imageUrl"
+              type="file"
+              placeholder="janesmith"
+              className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            />
+
+          </div>
+        </div>
+        
+      </div>
+      </div>
+      <button type="button"  onClick={posthandle} className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">送信</button>
+  </div>
     )
 }
 export default PersonalForm
